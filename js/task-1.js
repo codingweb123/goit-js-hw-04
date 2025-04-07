f38ae9f4a9df715beb4108f90f470ab4eb48efd5
staffm3 <staffm37@gmail.com>
2025-04-07 19:44:15 +0300

    fix task-1 & task done

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 const isEnoughCapacity = (products, containerSize) => {
-  return Object.values(products).reduce((a, b) => a + b, 0) < containerSize
+  return Object.values(products).reduce((a, b) => a + b, 0) <= containerSize
 }
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)) // true
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)) // false
